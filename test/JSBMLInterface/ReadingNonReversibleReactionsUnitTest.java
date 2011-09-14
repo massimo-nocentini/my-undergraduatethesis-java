843bf2940ef9a26c620919c8514cf225bc43c4f5
Massimo Nocentini <massimo.nocentini@gmail.com>
2011-09-14 19:22:50 +0200

    Added a more greedy test for reading a reaction with more than one reactant and more than one product (for now all these species are different)

--- test/JSBMLInterface/ReadingNonReversibleReactionsUnitTest.java
+++ test/JSBMLInterface/ReadingNonReversibleReactionsUnitTest.java
@@ -73,6 +73,144 @@ public class ReadingNonReversibleReactionsUnitTest {
 	}
 
 	@Test
+	public void checkVerticesCreationBasedOnManyToManyReaction() {
+		Connector connector = Connector.makeConnector();
+
+		Model sbmlModel = new Model();
+
+		Compartment compartment = sbmlModel.createCompartment("compartment_id");
+
+		String idReactant = "id_Reactor_";
+		String idProduct = "id_Product_";
+		Species reactant1 = sbmlModel.createSpecies(idReactant + "1",
+				compartment);
+		Species reactant2 = sbmlModel.createSpecies(idReactant + "2",
+				compartment);
+		Species product1 = sbmlModel
+				.createSpecies(idProduct + "1", compartment);
+		Species product2 = sbmlModel
+				.createSpecies(idProduct + "2", compartment);
+		Species product3 = sbmlModel
+				.createSpecies(idProduct + "3", compartment);
+
+		Reaction reaction = sbmlModel.createReaction("reaction_id");
+		reaction.setReversible(false);
+
+		// add a test in the learning test to keep the species reference
+		reaction.createReactant(reactant1);
+		reaction.createReactant(reactant2);
+		reaction.createProduct(product1);
+		reaction.createProduct(product2);
+		reaction.createProduct(product3);
+
+		final Set<Vertex> reactants = new HashSet<Vertex>();
+		final Set<Vertex> products = new HashSet<Vertex>();
+
+		final Set<Vertex> collectedOneByOneVertices = new HashSet<Vertex>();
+		Set<Vertex> vertices = connector.readReaction(reaction,
+				new VertexGenerationWithSourceListener() {
+
+					@Override
+					public void newVertexGenerated(Vertex vertex) {
+						collectedOneByOneVertices.add(vertex);
+					}
+
+					@Override
+					public void newVertexFromReactor(Vertex vertex) {
+						reactants.add(vertex);
+					}
+
+					@Override
+					public void newVertexFromProduct(Vertex vertex) {
+						products.add(vertex);
+					}
+				});
+
+		assertEquals(5, vertices.size());
+		assertEquals(collectedOneByOneVertices, vertices);
+
+		assertEquals(2, reactants.size());
+		assertEquals(3, products.size());
+
+		for (Vertex vertex : reactants) {
+			assertTrue(vertex.isYourNeighborhoodEquals(products));
+		}
+
+		for (Vertex vertex : products) {
+			assertTrue(vertex.isYourNeighborhoodEmpty());
+		}
+	}
+
+	@Test
+	public void checkVerticesCreationBasedOnManyToManyReactionWithOneSpeciesInCommon() {
+		Connector connector = Connector.makeConnector();
+
+		Model sbmlModel = new Model();
+
+		Compartment compartment = sbmlModel.createCompartment("compartment_id");
+
+		String idReactant = "id_Reactor_";
+		String idProduct = "id_Product_";
+		Species reactant1 = sbmlModel.createSpecies(idReactant + "1",
+				compartment);
+		Species reactant2 = sbmlModel.createSpecies(idReactant + "2",
+				compartment);
+		Species product1 = sbmlModel
+				.createSpecies(idProduct + "1", compartment);
+		Species product2 = sbmlModel
+				.createSpecies(idProduct + "2", compartment);
+		Species product3 = sbmlModel
+				.createSpecies(idProduct + "3", compartment);
+
+		Reaction reaction = sbmlModel.createReaction("reaction_id");
+		reaction.setReversible(false);
+
+		// add a test in the learning test to keep the species reference
+		reaction.createReactant(reactant1);
+		reaction.createReactant(reactant2);
+		reaction.createProduct(product1);
+		reaction.createProduct(product2);
+		reaction.createProduct(product3);
+
+		final Set<Vertex> reactants = new HashSet<Vertex>();
+		final Set<Vertex> products = new HashSet<Vertex>();
+
+		final Set<Vertex> collectedOneByOneVertices = new HashSet<Vertex>();
+		Set<Vertex> vertices = connector.readReaction(reaction,
+				new VertexGenerationWithSourceListener() {
+
+					@Override
+					public void newVertexGenerated(Vertex vertex) {
+						collectedOneByOneVertices.add(vertex);
+					}
+
+					@Override
+					public void newVertexFromReactor(Vertex vertex) {
+						reactants.add(vertex);
+					}
+
+					@Override
+					public void newVertexFromProduct(Vertex vertex) {
+						products.add(vertex);
+					}
+				});
+
+		assertEquals(5, vertices.size());
+		assertEquals(collectedOneByOneVertices, vertices);
+
+		assertEquals(2, reactants.size());
+		assertEquals(3, products.size());
+
+		for (Vertex vertex : reactants) {
+			assertTrue(vertex.isYourNeighborhoodEquals(products));
+		}
+
+		for (Vertex vertex : products) {
+			assertTrue(vertex.isYourNeighborhoodEmpty());
+		}
+	}
+
+	@Test
 	public void checkVertexSelfLoopInOneToOneReaction() {
 		Connector connector = Connector.makeConnector();
 
@@ -125,60 +263,5 @@ public class ReadingNonReversibleReactionsUnitTest {
 
 		assertTrue(((Vertex) (reactants.toArray()[0])).haveYouSelfLoop());
 	}
-
-	// @Test
-	// public void checkVerticesCreationBasedOnOneToOneReaction() {
-	// Connector connector = Connector.makeConnector();
-	//
-	// Model sbmlModel = new Model();
-	//
-	// Compartment compartment = sbmlModel.createCompartment("compartment_id");
-	//
-	// String idReactant = "id1";
-	// String idProduct = "id2";
-	// Species reactant = sbmlModel.createSpecies(idReactant, compartment);
-	// Species product = sbmlModel.createSpecies(idProduct, compartment);
-	//
-	// Reaction reaction = sbmlModel.createReaction("reaction_id");
-	// reaction.setReversible(false);
-	//
-	// // add a test in the learning test to keep the species reference
-	// reaction.createReactant(reactant);
-	// reaction.createProduct(product);
-	//
-	// final Set<Vertex> reactants = new HashSet<Vertex>();
-	// final Set<Vertex> products = new HashSet<Vertex>();
-	//
-	// Set<Vertex> vertices = connector.readReaction(reaction,
-	// new VertexGenerationWithSourceListener() {
-	//
-	// @Override
-	// public void newVertexGenerated(Vertex vertex) {
-	// }
-	//
-	// @Override
-	// public void newVertexFromReactor(Vertex vertex) {
-	// reactants.add(vertex);
-	// }
-	//
-	// @Override
-	// public void newVertexFromProduct(Vertex vertex) {
-	// products.add(vertex);
-	// }
-	// });
-	//
-	// assertEquals(2, vertices.size());
-	//
-	// assertEquals(1, reactants.size());
-	// assertEquals(1, products.size());
-	//
-	// for (Vertex vertex : reactants) {
-	// assertTrue(vertex.isYourNeighborhoodEquals(products));
-	// }
-	//
-	// for (Vertex vertex : products) {
-	// assertTrue(vertex.isYourNeighborhoodEmpty());
-	// }
-	// }
 
 }
