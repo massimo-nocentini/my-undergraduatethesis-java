ee0bf41780df6ea3dfae82eb642cab1e3ef1535d
Massimo Nocentini <massimo.nocentini@gmail.com>
2011-09-16 16:12:49 +0200

    Added two more tests for the reading of two reactions (this time reversible ones)

--- test/JSBMLInterface/ReadingSetOfReactions.java
+++ test/JSBMLInterface/ReadingSetOfReactions.java
@@ -19,7 +19,7 @@ import org.sbml.jsbml.Species;
 public class ReadingSetOfReactions {
 
 	@Test
-	public void twoReactionsWithNothingInCommon() {
+	public void twoIrreversibleReactionsWithNothingInCommon() {
 		Connector connector = Connector.makeConnector();
 
 		Model sbmlModel = new Model();
@@ -106,7 +106,107 @@ public class ReadingSetOfReactions {
 	}
 
 	@Test
-	public void twoReactionsWithSpeciesInCommonOnReactants() {
+	public void twoReversibleReactionsWithNothingInCommon() {
+		Connector connector = Connector.makeConnector();
+
+		Model sbmlModel = new Model();
+
+		Map<Vertex, Set<Vertex>> reactantsCheckingMap = new HashMap<Vertex, Set<Vertex>>();
+		Map<Vertex, Set<Vertex>> productsCheckingMap = new HashMap<Vertex, Set<Vertex>>();
+
+		Set<Vertex> setOfProductsOfFirstReaction = new HashSet<Vertex>();
+		Set<Vertex> setOfProductsOfSecondReaction = new HashSet<Vertex>();
+		Set<Vertex> setOfReactantsOfFirstReaction = new HashSet<Vertex>();
+		Set<Vertex> setOfReactantsOfSecondReaction = new HashSet<Vertex>();
+
+		Compartment compartment = sbmlModel.createCompartment("compartment_id");
+
+		Species reactant1 = sbmlModel.createSpecies("idR1", compartment);
+		Species reactant2 = sbmlModel.createSpecies("idR2", compartment);
+		Species reactant3 = sbmlModel.createSpecies("idR3", compartment);
+		Species product1 = sbmlModel.createSpecies("idP1", compartment);
+		Species product2 = sbmlModel.createSpecies("idP2", compartment);
+		Species product3 = sbmlModel.createSpecies("idP3", compartment);
+
+		Vertex vR1 = Vertex.makeVertex(reactant1);
+		Vertex vR2 = Vertex.makeVertex(reactant2);
+		Vertex vR3 = Vertex.makeVertex(reactant3);
+		Vertex vP1 = Vertex.makeVertex(product1);
+		Vertex vP2 = Vertex.makeVertex(product2);
+		Vertex vP3 = Vertex.makeVertex(product3);
+
+		setOfProductsOfFirstReaction.add(vP1);
+		setOfProductsOfSecondReaction.add(vP2);
+		setOfProductsOfSecondReaction.add(vP3);
+
+		setOfReactantsOfFirstReaction.add(vR1);
+		setOfReactantsOfFirstReaction.add(vR2);
+		setOfReactantsOfSecondReaction.add(vR3);
+
+		reactantsCheckingMap.put(vR1, setOfProductsOfFirstReaction);
+		reactantsCheckingMap.put(vR2, setOfProductsOfFirstReaction);
+		reactantsCheckingMap.put(vR3, setOfProductsOfSecondReaction);
+
+		productsCheckingMap.put(vP1, setOfReactantsOfFirstReaction);
+		productsCheckingMap.put(vP2, setOfReactantsOfSecondReaction);
+		productsCheckingMap.put(vP3, setOfReactantsOfSecondReaction);
+
+		Reaction reaction = sbmlModel.createReaction("reaction1_id");
+		reaction.setReversible(true);
+
+		// add a test in the learning test to keep the species reference
+		reaction.createReactant(reactant1);
+		reaction.createReactant(reactant2);
+		reaction.createProduct(product1);
+
+		Reaction anotherReaction = sbmlModel.createReaction("reaction2_id");
+		anotherReaction.setReversible(true);
+
+		// add a test in the learning test to keep the species reference
+		anotherReaction.createReactant(reactant3);
+		anotherReaction.createProduct(product2);
+		anotherReaction.createProduct(product3);
+
+		final Set<Vertex> reactants = new HashSet<Vertex>();
+		final Set<Vertex> products = new HashSet<Vertex>();
+
+		Set<Vertex> vertices = connector.readReactions(
+				sbmlModel.getListOfReactions(),
+				new VertexHandlingWithSourceListener() {
+
+					@Override
+					public void vertexHandled(Vertex vertex) {
+					}
+
+					@Override
+					public void reactantVertexHandled(Vertex vertex) {
+						reactants.add(vertex);
+					}
+
+					@Override
+					public void productVertexHandled(Vertex vertex) {
+						products.add(vertex);
+					}
+				});
+
+		assertEquals(6, vertices.size());
+
+		assertEquals(3, reactants.size());
+		assertEquals(3, products.size());
+
+		for (Vertex vertex : reactants) {
+			assertTrue(vertex.isYourNeighborhoodEquals(reactantsCheckingMap
+					.get(vertex)));
+		}
+
+		for (Vertex vertex : products) {
+			assertTrue(vertex.isYourNeighborhoodEquals(productsCheckingMap
+					.get(vertex)));
+		}
+	}
+
+	@Test
+	public void twoIrreversibleReactionsWithSpeciesInCommonOnReactants() {
 		Connector connector = Connector.makeConnector();
 
 		Model sbmlModel = new Model();
@@ -201,6 +301,128 @@ public class ReadingSetOfReactions {
 
 		for (Vertex vertex : products) {
 			assertTrue(vertex.isYourNeighborhoodEmpty());
+		}
+	}
+
+	@Test
+	public void twoReversibleReactionsWithSpeciesInCommonOnReactants() {
+		Connector connector = Connector.makeConnector();
+
+		Model sbmlModel = new Model();
+
+		Map<Vertex, Set<Vertex>> reactantsCheckingMap = new HashMap<Vertex, Set<Vertex>>();
+		Map<Vertex, Set<Vertex>> productsCheckingMap = new HashMap<Vertex, Set<Vertex>>();
+
+		Set<Vertex> setOfProductsOfFirstReaction = new HashSet<Vertex>();
+		Set<Vertex> setOfProductsOfSecondReaction = new HashSet<Vertex>();
+		Set<Vertex> setOfReactantsOfFirstReaction = new HashSet<Vertex>();
+		Set<Vertex> setOfReactantsOfSecondReaction = new HashSet<Vertex>();
+
+		Compartment compartment = sbmlModel.createCompartment("compartment_id");
+
+		Species reactant1 = sbmlModel.createSpecies("idR1", compartment);
+		Species reactant2 = sbmlModel.createSpecies("idR2", compartment);
+
+		// common species
+		Species reactant3 = sbmlModel.createSpecies("idR3", compartment);
+
+		Species reactant4 = sbmlModel.createSpecies("idR4", compartment);
+		Species product1 = sbmlModel.createSpecies("idP1", compartment);
+		Species product2 = sbmlModel.createSpecies("idP2", compartment);
+		Species product3 = sbmlModel.createSpecies("idP3", compartment);
+
+		Vertex vR1 = Vertex.makeVertex(reactant1);
+		Vertex vR2 = Vertex.makeVertex(reactant2);
+		Vertex vR3 = Vertex.makeVertex(reactant3);
+		Vertex vR4 = Vertex.makeVertex(reactant4);
+		Vertex vP1 = Vertex.makeVertex(product1);
+		Vertex vP2 = Vertex.makeVertex(product2);
+		Vertex vP3 = Vertex.makeVertex(product3);
+
+		setOfProductsOfFirstReaction.add(vP1);
+		setOfProductsOfSecondReaction.add(vP2);
+		setOfProductsOfSecondReaction.add(vP3);
+
+		setOfReactantsOfFirstReaction.add(vR1);
+		setOfReactantsOfFirstReaction.add(vR2);
+		setOfReactantsOfFirstReaction.add(vR3);
+		setOfReactantsOfSecondReaction.add(vR3);
+		setOfReactantsOfSecondReaction.add(vR4);
+
+		productsCheckingMap.put(vP1, setOfReactantsOfFirstReaction);
+		productsCheckingMap.put(vP2, setOfReactantsOfSecondReaction);
+		productsCheckingMap.put(vP3, setOfReactantsOfSecondReaction);
+
+		Set<Vertex> setOfProductsOfCommonSpecies = new HashSet<Vertex>();
+		setOfProductsOfCommonSpecies.addAll(setOfProductsOfFirstReaction);
+		setOfProductsOfCommonSpecies.addAll(setOfProductsOfSecondReaction);
+
+		// Set<Vertex> setOfReactantsOfCommonSpecies = new HashSet<Vertex>();
+		// setOfReactantsOfCommonSpecies.addAll(setOfReactantsOfFirstReaction);
+		// setOfReactantsOfCommonSpecies.addAll(setOfReactantsOfSecondReaction);
+
+		reactantsCheckingMap.put(vR1, setOfProductsOfFirstReaction);
+		reactantsCheckingMap.put(vR2, setOfProductsOfFirstReaction);
+		reactantsCheckingMap.put(vR3, setOfProductsOfCommonSpecies);
+		reactantsCheckingMap.put(vR4, setOfProductsOfSecondReaction);
+
+		productsCheckingMap.put(vP1, setOfReactantsOfFirstReaction);
+		productsCheckingMap.put(vP2, setOfReactantsOfSecondReaction);
+		productsCheckingMap.put(vP3, setOfReactantsOfSecondReaction);
+
+		Reaction reaction = sbmlModel.createReaction("reaction1_id");
+		reaction.setReversible(true);
+
+		// add a test in the learning test to keep the species reference
+		reaction.createReactant(reactant1);
+		reaction.createReactant(reactant2);
+		reaction.createReactant(reactant3);
+		reaction.createProduct(product1);
+
+		Reaction anotherReaction = sbmlModel.createReaction("reaction2_id");
+		anotherReaction.setReversible(true);
+
+		// add a test in the learning test to keep the species reference
+		anotherReaction.createReactant(reactant3);
+		anotherReaction.createReactant(reactant4);
+		anotherReaction.createProduct(product2);
+		anotherReaction.createProduct(product3);
+
+		final Set<Vertex> reactants = new HashSet<Vertex>();
+		final Set<Vertex> products = new HashSet<Vertex>();
+
+		Set<Vertex> vertices = connector.readReactions(
+				sbmlModel.getListOfReactions(),
+				new VertexHandlingWithSourceListener() {
+
+					@Override
+					public void vertexHandled(Vertex vertex) {
+					}
+
+					@Override
+					public void reactantVertexHandled(Vertex vertex) {
+						reactants.add(vertex);
+					}
+
+					@Override
+					public void productVertexHandled(Vertex vertex) {
+						products.add(vertex);
+					}
+				});
+
+		assertEquals(7, vertices.size());
+
+		assertEquals(4, reactants.size());
+		assertEquals(3, products.size());
+
+		for (Vertex vertex : reactants) {
+			assertTrue(vertex.isYourNeighborhoodEquals(reactantsCheckingMap
+					.get(vertex)));
+		}
+
+		for (Vertex vertex : products) {
+			assertTrue(vertex.isYourNeighborhoodEquals(productsCheckingMap
+					.get(vertex)));
 		}
 	}
 }
